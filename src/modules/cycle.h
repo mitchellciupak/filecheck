46c685a7a617a4aaa8158397a5d8f16473b16bf9
mitchellciupak <ciupak@purdue.edu>
2021-04-30 13:44:08 -0400

    error

--- src/modules/cycle.h
+++ src/modules/cycle.h
@@ -9,6 +9,6 @@ int isFolderSymLink(int id);
 int getSymLinkID(int id);
 void checkForCycles(folder * parent);
 void printFolderStructure(folder * parent);
-void folderfree(folder * top)
+void folderfree(folder * top);
 
 #endif
