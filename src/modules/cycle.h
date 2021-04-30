43f84bf75a5d85ac3914f36eaef0df1cbbfac1b6
mitchellciupak <ciupak@purdue.edu>
2021-04-30 13:43:42 -0400

    cycle.h upated

--- src/modules/cycle.h
+++ src/modules/cycle.h
@@ -4,6 +4,11 @@
 #include "../shared.h"
 
 int executeCycleCheck(char * filename);
+int getNumChildren(int id);
+int isFolderSymLink(int id);
+int getSymLinkID(int id);
+void checkForCycles(folder * parent);
+void printFolderStructure(folder * parent);
 void folderfree(folder * top)
 
 #endif
