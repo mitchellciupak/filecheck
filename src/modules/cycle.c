7c143e2b368624fe9430607a6768875038c25fed
mitchellciupak <ciupak@purdue.edu>
2021-04-30 13:10:05 -0400

    laptop keeps freezing, commiting regularly

--- src/modules/cycle.c
+++ src/modules/cycle.c
@@ -1,8 +1,9 @@
 #include "cycle.h"
 
 typedef struct {
-    int isTopParentDir;//0 or 1
+    int id;
     int isSymLink;  //0 or 1
+    int SymLinkID;
     int numChildren;//0 to INT_MAX
     folder * childrenArr;
 } folder;
@@ -13,6 +14,13 @@ typedef struct {
 int executeCycleCheck(char * filename) {
 
     folder * parent = malloc(1 * sizeof(folder));
+    parent->id = 0;
+
+    ////////// FROM HERE THIS IS A HYPOTHETICAL TEST ///////////
+    parent->numChildren = 3;
+    parent->childrenArr = allocateSubFolders(parent);
+
+    ////////// END OF HYPOTHETICAL TEST ///////////
 
     //populate folder strucutes all the way down
 
@@ -20,6 +28,26 @@ int executeCycleCheck(char * filename) {
 
     folderfree(parent);
     return RETURN_FAILURE;
+}
+
+folder * allocateSubFolders(folder * parent){
+    int i = 0;
+    folder * tempArr = malloc(parent->numChildren * sizeof(folder));
+
+    for(i = 0; i < parent->numChildren; i++){
+        tempArr[i].id = parent->id + i;
+        tempArr[i].numChildren = getNumChildren(tempArr[i].id);
+
+        if(isFolderSymLink(tempArr[i].id)){
+            tempArr[i].isSymLink = 1;
+            tempArr[i].SymLinkID = getSymLinkID(tempArr[i].id);
+        } else {
+            tempArr[i].isSymLink = 0; //False
+            tempArr[i].SymLinkID = -1;//Flase
+        }
+    }
+
+    return tempArr;
 }
 
 void folderfree(folder * top){
@@ -35,7 +63,7 @@ void folderfree(folder * top){
     }
 
     //Free top most
-    if(top->isTopParentDir == 1){
+    if(top->id == 0){
         free(top);
     }
 
